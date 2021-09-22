3896d3a1ffefe5ee5629ae1e421e31983289d675
Michael Chong <mikec0920@gmail.com>
2021-09-21 17:06:59 -0700

    Implemented simple gameplay

--- hangman.rb
+++ hangman.rb
@@ -1,16 +1,68 @@
+class Dictionary
+  attr_reader :secret_word
 
-class Game
   def initialize
     file = File.open('5desk.txt', 'r')
-    dictionary = file.readlines.map { |word| word.strip }
-    @filtered_dictionary = dictionary.filter do |word|
+    word_list = file.readlines.map { |word| word.strip.downcase }
+    @dictionary = word_list.filter do |word|
       word.length > 5 && word.length < 12
     end
+    @secret_word = @dictionary.sample
   end
 
   def new_secret_word
-    @secret_word = @filtered_dictionary.sample
+    @secret_word = @dictionary.sample
   end
 end
 
-p Game.new.new_secret_word
+class Player
+  def initialize
+    @name = ''
+  end
+
+  def name?
+    print 'What is your name? (default: Player) '
+    name = gets.chomp
+    @name = name.empty? ? 'Player' : name
+  end
+
+  def guess
+    print 'Which letter do you think the word includes? '
+    while (letter = gets.chomp)
+      break if letter.length == 1 && /[[:alpha:]]/ =~ letter
+
+      print 'Invalid input. Enter a single alphabetic charcter: '
+    end
+    letter
+  end
+end
+
+class Game
+  def initialize
+    @dictionary = Dictionary.new
+    @player = Player.new
+    @correct_letters = []
+    @num_wrong_guesses = 0
+  end
+
+  def check_guess(letter)
+    if @dictionary.secret_word.chars.include?(letter)
+      @correct_letters.include?(letter) ? return : @correct_letters << letter
+    else
+      @num_wrong_guesses += 1
+    end
+  end
+
+  def play
+    @player.name?
+    until @num_wrong_guesses == 6 || @correct_letters.sort == @dictionary.secret_word.chars.uniq.sort
+      print @dictionary.secret_word
+      letter = @player.guess
+      check_guess(letter)
+      puts "Number of wrong guesses: #{@num_wrong_guesses}"
+    end
+    @num_wrong_guesses == 6 ? 'You lose!' : 'You win!'
+  end
+end
+
+p Game.new.play
